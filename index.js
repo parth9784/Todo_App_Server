4857847c149df37199e859dcd5983125491a295d
parth9784 <pdsharma458@gmail.com>
2024-07-20 01:36:04 +0530

    added cors

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ require("dotenv").config()
 const app=express()
 const dbconnect=require("./config/database")
 const routes=require("./routes/Routes")
+const cors=require("cors")
 dbconnect()
 app.use(express.json())
 app.use("",routes);
-
+app.use(cors())
 app.get("/",(req,res)=>{
     res.send("<h1>This is Todo App API Developed By Parth Dadhich..</h1>")
 })
